914721731929d3985eb8c9f1ce03a4dd70bef493
Leticia Karoline <lele202@hotmail.com.br>
2022-11-23 21:15:34 -0300

    Primeiro Teste

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Trabalho___Turma_23.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
